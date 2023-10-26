3d0233c2be0a6d6277d3429532efd9654c37707d
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-10-25 22:47:09 -0400

    finish up

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
+import { addUser } from '../thunks/addUser';
+import { removeUser } from '../thunks/removeUser';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    data: [],
+    error: null,
+    isLoading: false,
+  },
+  extraReducers(builder) {
+    // watch for 3 cases.
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+
+    builder.addCase(addUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(addUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.push(action.payload);
+    });
+    builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+
+    builder.addCase(removeUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(removeUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      // find the user that was deleted and filter that!
+      // FIX ME
+      state.data = state.data.filter((user) => {
+        return user.id !== action.payload.id;
+      });
+    });
+    builder.addCase(removeUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
+});
+
+export const usersReducer = usersSlice.reducer;
